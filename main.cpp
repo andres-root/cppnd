ca901f214b0e160a67c31c93cd28aaaa53ea57dd
andres-root <andreslujandev@gmail.com>
2024-10-24 11:12:36 -0500

    WIP

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using std::cout;
+
+int main() {
+    int a = 9;
+
+    std::string b;
+
+    b = "Hello World!";
+
+    cout << a << "\n";
+    cout << b << "\n";
+    return 0;
+}
---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+int main() {
+    auto i = 5;
+    auto v_6 = {4, 5, 6};
+    // Vector
+    vector<int> a;
+    vector<vector<int>> v = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    return 0;
+}
